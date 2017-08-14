554f0cd6aed862d3f979816ef508d9e4b1302560
rajatmyname <30835221+rajatmyname@users.noreply.github.com>
2017-08-14 20:10:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
@@ -12,9 +16,6 @@ app.get('/article-two',function(req,res){
 });
 app.get('/article-three',function(req,res){
     res.send("Article three requested and served here");
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
