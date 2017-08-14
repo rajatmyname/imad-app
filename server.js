e0862f1fe2e5ef46d1df9427edfcf50f8a789735
rajatmyname <30835221+rajatmyname@users.noreply.github.com>
2017-08-14 20:07:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 app.get('/article-one',function(req,res){
-    res.send("Article one requested and served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send("Article two requested and served here");
