ea45e7ba355139ddfa9a85d9c8a68517083ae554
rajatmyname <30835221+rajatmyname@users.noreply.github.com>
2017-08-16 09:22:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 var button=document.getElementById('counter');
-var counter=0;
-button.onClick=function(){
+
+button.onclick=function(){
+    var request= new XMLHttpRequest();
+    request.onreadystateChange = function(){
+      if(request.readystate==XMLHttpRequest.DONE){
+          if(request.status==200){
+              var counter = request.responseText;
+              var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+          }
+      }  
+    };
   counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  request.open('GET','http://rc10131.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
